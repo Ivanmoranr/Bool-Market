30f8c29f294ec62d9fb030d5cb4d65e0a87642f1
Miguel-Equiza <miguelequiza12345@gmail.com>
2023-09-02 12:29:53 +0200

    Initial single and hacking models

--- get_real_data.py
+++ get_real_data.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
-def get_real_X_y():
+def get_real_X_y(noise=False):
     pattern_list = ["rising_wedge", "falling_wedge", "double_top", "double_bottom"]
+
     X1=[]
     y1=[]
     X2=[]
@@ -37,6 +38,40 @@ def get_real_X_y():
                     df = pd.read_csv(f"data/patterns/double_bottom/{filename}")
                     X4.append(np.array(df[["Open", "High", "Low", "Close"]]))
                     y4.append(tuple(df.iloc[0][["Start Date", "End Date", "Pattern"]]))
+
+    if noise:
+        for i in range(len(X1)):
+            pat = {"0":X2, "1":X3,"2":X4}
+            p = np.random.randint(0,3)
+            p = str(p)
+            num = np.random.randint(0,len(pat[p]))
+            X1.append(pat[p][num])
+            y1.append([-1,-1,0])
+
+        for i in range(len(X2)):
+            pat = {"0":X1, "1":X3,"2":X4}
+            p = np.random.randint(0,3)
+            p = str(p)
+            num = np.random.randint(0,len(pat[p]))
+            X2.append(pat[p][num])
+            y2.append([-1,-1,0])
+
+        for i in range(len(X3)):
+            pat = {"0":X1, "1":X2,"2":X4}
+            p = np.random.randint(0,3)
+            p = str(p)
+            num = np.random.randint(0,len(pat[p]))
+            X3.append(pat[p][num])
+            y3.append([-1,-1,0])
+
+        for i in range(len(X4)):
+            pat = {"0":X1, "1":X2,"2":X3}
+            p = np.random.randint(0,3)
+            p = str(p)
+            num = np.random.randint(0,len(pat[p]))
+            X4.append(pat[p][num])
+            y4.append([-1,-1,0])
+
 
     return X1, y1, X2, y2, X3, y3, X4, y4
 
