ca21e16f9ee3fb95cc1913a20831934c62e859e1
Miguel-Equiza <miguelequiza12345@gmail.com>
2023-09-05 19:07:48 +0200

    Changes in the model

--- preprocessing.py
+++ preprocessing.py
@@ -22,48 +22,23 @@ def preprocess(X, y):
         return y_s
 
     y_preprocessed = onehotencode(y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y_preprocessed, test_size=0.2)
 
-    y_train_dates = y_train[:,:2]
-    y_test_dates = y_test[:,:2]
-    y_train_p = y_train[:,2:]
-    y_test_p = y_test[:,2:]
+    y_dates = y_preprocessed[:,:2]
+    y_p = y_preprocessed[:,2:]
 
     X_train_processed=[]
     scale={}
-    for i in range(len(X_train)):
+    for i in range(len(X)):
         open_scaler_i = StandardScaler()
-        X_train_processed.append(open_scaler_i.fit_transform(X_train[i]))
-        scale[f"open_scaler_{len(X_train[i])}"] = open_scaler_i
+        X_train_processed.append(open_scaler_i.fit_transform(X[i]))
+        scale[f"open_scaler_{len(X[i])}"] = open_scaler_i
 
-    X_test_processed=[]
-    scale={}
-    for i in range(len(X_test)):
-        open_scaler_i = StandardScaler()
-        X_test_processed.append(open_scaler_i.fit_transform(X_test[i]))
-        scale[f"open_scaler_{len(X_test[i])}"] = open_scaler_i
-
-    x=[]
-
-    for i in range(len(X_train)):
-        x.append(len(X_train[i]))
-
-    xx=[]
-
-    for i in range(len(X_test_processed)):
-        xx.append(len(X_test_processed[i]))
-
-    maxlen = max(max(x), max(xx))
+    maxlen = 450
 
     X_train_preprocessed = pad_sequences(X_train_processed, maxlen = maxlen, dtype='float32', padding='post', value=-1)
-    X_test_preprocessed = pad_sequences(X_test_processed, maxlen = maxlen, dtype='float32', padding='post', value=-1)
 
     X_train_preprocessed = tf.convert_to_tensor(X_train_preprocessed, np.float32)
-    y_train_p = tf.convert_to_tensor(y_train_p, np.int16)
-    y_train_dates = tf.convert_to_tensor(y_train_dates, np.int16)
+    y_train_p = tf.convert_to_tensor(y_p, np.int16)
+    y_train_dates = tf.convert_to_tensor(y_dates, np.int16)
 
-    X_test_preprocessed = tf.convert_to_tensor(X_test_preprocessed, np.float32)
-    y_test_p = tf.convert_to_tensor(y_test_p, np.int16)
-    y_test_dates = tf.convert_to_tensor(y_test_dates, np.int16)
-
-    return X_train_preprocessed, y_train_p, y_train_dates, X_test_preprocessed, y_test_p, y_test_dates
+    return X_train_preprocessed, y_train_p, y_train_dates
